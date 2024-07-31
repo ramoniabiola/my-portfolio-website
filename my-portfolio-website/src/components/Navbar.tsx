8dcc1bebd92de306334d4c55afc2d79aa95c3615
ramoni61 <ramoniabiola61@gmail.com>
2024-07-31 23:07:51 +0100

    adjusted the hover style of the navbar

--- my-portfolio-website/src/components/Navbar.tsx
+++ my-portfolio-website/src/components/Navbar.tsx
@@ -6,9 +6,9 @@ export const Navbar = () => {
             <h1 className="ml-32 text-emerald-500 text-4xl font-bold  font-mono cursor-pointer">
                 Ram<span className="text-4xl font-bold font-sans text-emerald-200">o</span>ni
             </h1>
-            <ul className="flex items-center justify-center text-gray-400 mr-24 font-medium text-xl  cursor-pointer">
+            <ul className="flex items-center justify-center text-gray-400 mr-24 font-bold text-xl  cursor-pointer">
                 {['Home', 'About', 'Portfolio', 'Services', 'Experience', 'Contact'].map((item, index) => (
-                  <li key={index} className="relative mr-14 py-3 after:content-[''] after:h-1 after:w-0 after:bg-emerald-600 after:rounded after:absolute after:left-0 after:bottom-0 after:transition-all after:duration-600 after:ease-in-out after:transform after:-translate-y-0 hover:after:w-full hover:text-gray-100">
+                  <li key={index} className="mr-14 hover:text-emerald-600">
                     {item}
                   </li>
                 ))}
